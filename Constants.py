2c2d58bd433ac3fb0e74ed4dd09ac036f3723281
gvalliap <ganeshv757@gmail.com>
2019-08-18 13:26:45 -0700

    add constants file

---
+++ Constants.py
@@ -0,0 +1,8 @@
+COLUMNS = ['First Name', 'Last Name', 'Class Standing', 'Cum GPA', 'Major Code', 'Dept']
+DEPTS = ['CSE', 'ECE', 'MATH', 'BENG']
+CLASS_STANDINGS = ['SO', 'JR', 'SR']
+DEPTS_MAJORS = dict()  # bit of a faux-pas...
+DEPTS_MAJORS['CSE'] = ['CS25', 'CS26', 'CS27', 'CS28']
+DEPTS_MAJORS['ECE'] = ['EC26', 'EC27', 'EC28']
+DEPTS_MAJORS['MATH'] = ['MA30']
+DEPTS_MAJORS['BENG'] = ['BE25', 'BE26', 'BE27', 'BE28']
