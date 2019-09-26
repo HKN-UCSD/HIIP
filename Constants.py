f71d8e11ede07c4ab1482ebe6be72243093acf4b
gvalliap <ganeshv757@gmail.com>
2019-09-25 22:40:21 -0700

    added filtering

--- Constants.py
+++ Constants.py
@@ -1,4 +1,4 @@
-COLUMNS = ['First Name', 'Last Name', 'Class Standing', 'Cum GPA', 'Major Code', 'Dept']
+COLUMNS = ['First Name', 'Last Name', 'Class Standing', 'Cum GPA', 'Major Code', 'Dept', 'Email']
 DEPTS = ['CSE', 'ECE', 'MATH', 'BENG']
 CLASS_STANDINGS = ['SO', 'JR', 'SR']
 DEPTS_MAJORS = dict()  # bit of a faux-pas...
@@ -6,3 +6,4 @@ DEPTS_MAJORS['CSE'] = ['CS25', 'CS26', 'CS27', 'CS28']
 DEPTS_MAJORS['ECE'] = ['EC26', 'EC27', 'EC28']
 DEPTS_MAJORS['MATH'] = ['MA30']
 DEPTS_MAJORS['BENG'] = ['BE25', 'BE26', 'BE27', 'BE28']
+CLASS_QUANTILE = {'SO': 0.8, 'JR': 0.75, 'SR': 0.667}
